a5d7b40f272a25a99b28b0e34a009036ab7fdc2a
Richard Sachek <rsachek@gmail.com>
2018-06-19 18:06:35 +0000

    Adding quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,5 +6,15 @@ end
 def new
   @quote = Quote.new
   end
+
+def create
+  Quote.create(quote_params)
+  redirect_to root_path
+end
   
+end
+
+private
+def quote_params
+  params.require(:quote).permit(:saying, :author)
 end
